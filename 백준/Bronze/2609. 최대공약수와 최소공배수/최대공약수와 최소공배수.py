34b28b816c196353186f644f8db794eeb2ae890c
ghdwlgns <50404133+ghdwlgns@users.noreply.github.com>
2025-02-05 04:49:25 +0900

    [Bronze I] Title: 최대공약수와 최소공배수, Time: 36 ms, Memory: 34536 KB -BaekjoonHub

--- 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.py
+++ 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.py
@@ -1,19 +1,9 @@
+import math
+
 m, n = map(int, input().split())
 
-def lcm(x, y):
-    (x, y) = (max(x, y), min(x, y))
-    (a, b) = (x, y)
-    
+gcd = math.gcd(m, n)
+lcm = (m * n) // gcd
 
-    while y > 0:
-        r = x % y
-        x, y = y, r
-    
-    g = x
-
-    return (a * b) // g, g
-
-l, g = lcm(m, n)
-
-print(g)
-print(l)
+print(gcd)
+print(lcm)
