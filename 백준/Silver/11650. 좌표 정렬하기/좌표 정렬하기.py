443dc1486f904b29231229eb9554aefa8a32f498
ghdwlgns <50404133+ghdwlgns@users.noreply.github.com>
2025-02-07 19:16:38 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 200 ms, Memory: 54680 KB -BaekjoonHub

--- 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.py
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.py
@@ -2,7 +2,7 @@ import sys
 
 n = int(input())
 
-point_list = [(x, y) for x, y in (map(int, sys.stdin.readline().strip().split()) for _ in range(n))]
+point_list = [(x, y) for x, y in (map(int, sys.stdin.readline().split()) for _ in range(n))]
 point_list.sort()
 
 print('\n'.join(f'{x} {y}' for (x, y) in point_list))
