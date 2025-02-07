95622aa00132009c7a0af6dd4ae5635e4e46294a
ghdwlgns <50404133+ghdwlgns@users.noreply.github.com>
2025-02-07 22:15:35 +0900

    [Silver III] Title: 피보나치 함수, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1003. 피보나치 함수/피보나치 함수.py
+++ 백준/Silver/1003. 피보나치 함수/피보나치 함수.py
@@ -1,9 +1,9 @@
 t = int(input())
 
-result = [(1, 0), (0, 1)]
+result = [(1, 0), (0, 1)] + [None] * 39
 
 for i in range(2, 41):
-    result.append(tuple(a + b for a, b in zip(result[i - 1], result[i - 2])))
+    result[i] = (result[i - 1][0] + result[i - 2][0], result[i - 1][1] + result[i - 2][1])
 
 for _ in range(t):
     print(*result[int(input())])
