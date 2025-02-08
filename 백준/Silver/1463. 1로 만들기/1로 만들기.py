58f3405703361b60efb5352233fe21fd6d687938
ghdwlgns <50404133+ghdwlgns@users.noreply.github.com>
2025-02-08 22:55:26 +0900

    [Silver III] Title: 1로 만들기, Time: 464 ms, Memory: 40224 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.py
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.py
@@ -1,11 +1,12 @@
-result = [None] + [0] * 1000000
+n = int(input())
 
-for i in range(2, 1000001):
+result = [0] * (n + 1)
+
+for i in range(2, n + 1):
     result[i] = result[i - 1] + 1
     if i % 2 == 0:
         result[i] = min(result[i], result[i // 2] + 1)
     if i % 3 == 0:
         result[i] = min(result[i], result[i // 3] + 1)
 
-n = int(input())
 print(result[n])
