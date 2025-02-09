02bb0f74061b2932e4cdd58929f3e4983a1b3de7
ghdwlgns <50404133+ghdwlgns@users.noreply.github.com>
2025-02-10 04:10:43 +0900

    [Gold V] Title: Z, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/1074. Z/Z.py
+++ 백준/Gold/1074. Z/Z.py
@@ -1,24 +1,14 @@
 n, r, c = map(int, input().split())
 
 result = 0
+size = 2 ** (n - 1) # 초기 탐색할 정사각형 배열(행렬) 한 변 길이
 
-while n >= 0:
-    if 0 <= r < 2 ** (n - 1) and 0 <= c < 2 ** (n - 1):
-        # 1사분면에 위치
-        pass
-    elif 0 <= r < 2 ** (n - 1) and 2 ** (n - 1) <= c < 2 ** n:
-        # 2 사분면에 위치
-        result += 4 ** (n - 1)
-        c -= 2 ** (n - 1)
-    elif 2 ** (n - 1) <= r < 2 ** n and 0 <= c < 2 ** (n - 1):
-        # 3 사분면에 위치
-        result += 2 * 4 ** (n - 1)
-        r -= 2 ** (n - 1)
-    else:
-        # 4 사분면에 위치
-        result += 3 * 4 ** (n - 1)
-        r -= 2 ** (n - 1)
-        c -= 2 ** (n - 1)
-    n -= 1
+while size > 0:
+    quadrant = (r >= size) * 2 + (c >= size)
+    result += quadrant * size ** 2
+    
+    r %= size
+    c %= size
+    size //= 2
 
 print(result)
