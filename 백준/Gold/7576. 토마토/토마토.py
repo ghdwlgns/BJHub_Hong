9dc8a6b1944e5ca3b37118f4349209ac7dfda959
ghdwlgns <50404133+ghdwlgns@users.noreply.github.com>
2025-02-10 10:47:51 +0900

    [Gold V] Title: 토마토, Time: 1060 ms, Memory: 100492 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.py
+++ 백준/Gold/7576. 토마토/토마토.py
@@ -8,37 +8,23 @@ directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
 
 def bfs():
     queue = deque()
-    for x in range(m):
-        for y in range(n):
-            if box_state[y][x] == 1:
-                queue.append((x, y))
+    
+    queue.extend((x, y) for y in range(n) for x in range(m) if box_state[y][x] == 1)
 
     while queue:
         x, y = queue.popleft()
 
         for dx, dy in directions:
-            if 0 <= x + dx < m and 0 <= y + dy < n and box_state[y + dy][x + dx] == 0:
-                box_state[y + dy][x + dx] = 1 + box_state[y][x]
-                queue.append((x + dx, y + dy))
+            nx, ny = x + dx, y + dy
+            if 0 <= nx < m and 0 <= ny < n and box_state[ny][nx] == 0:
+                box_state[ny][nx] = 1 + box_state[y][x]
+                queue.append((nx, ny))
 
-all_ripe = True
 result = 0
 
 bfs()
 
-for x in range(m):
-    for y in range(n):
-        if box_state[y][x] == 0:
-            all_ripe = False
-            break
-        elif box_state != -1:
-            result = max(result, box_state[y][x])
-
-    if not all_ripe:
-        break
-
-if not all_ripe:
+if any(0 in row for row in box_state):
     print(-1)
 else:
-    print(result - 1)
-    
+    print(max(map(max, box_state)) - 1)
