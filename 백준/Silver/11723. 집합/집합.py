5d0535fd81464f5ba55552ee8f31c75a554ae0bd
ghdwlgns <50404133+ghdwlgns@users.noreply.github.com>
2025-02-10 12:07:04 +0900

    [Silver V] Title: 집합, Time: 2900 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/11723. 집합/집합.py
+++ 백준/Silver/11723. 집합/집합.py
@@ -7,16 +7,16 @@ num_set = set()
 for _ in range(m):
     op = sys.stdin.readline()
     if 'add' in op:
-        x = int(op.split()[1])
+        x = int(op[4:])
         num_set.add(x)
     elif 'remove' in op:
-        x = int(op.split()[1])
+        x = int(op[7:])
         num_set.discard(x)
     elif 'check' in op:
-        x = int(op.split()[1])
+        x = int(op[6:])
         print(1 if x in num_set else 0)
     elif 'toggle' in op:
-        x = int(op.split()[1])
+        x = int(op[7:])
         if x in num_set:
             num_set.remove(x)
         else:
